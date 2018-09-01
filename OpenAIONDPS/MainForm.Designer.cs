cd83c9ab8a5f1631aeb20f271936df4fe8080b64
EL <ELPendragon3000@gmail.com>
2018-09-01 12:11:07 +0900

    Version 1.0.14

--- OpenAIONDPS/MainForm.Designer.cs
+++ OpenAIONDPS/MainForm.Designer.cs
@@ -719,7 +719,7 @@
             this.Controls.Add(this.StartButton);
             this.MaximumSize = new System.Drawing.Size(930, 860);
             this.Name = "MainForm";
-            this.Text = "Open AION DPS 1.0.13";
+            this.Text = "Open AION DPS 1.0.14";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.MemberGroupBox.ResumeLayout(false);
